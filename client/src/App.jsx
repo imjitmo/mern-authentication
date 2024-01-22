85c93afcf32e6a93de01f00a1017d65e4f0cbafc
jitmo_0421 <jerram0421@gmail.com>
2024-01-22 12:57:26 +0800

    added routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css';
 
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signin from './pages/signin';
+import Signup from './pages/signup';
+
 function App() {
   return (
-    <>
-      <div>App</div>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
