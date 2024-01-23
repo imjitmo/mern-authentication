4510bf2bd96caf481a71f13a0ad91d0ecf4ef65a
jitmo_0421 <jerram0421@gmail.com>
2024-01-23 11:02:08 +0800

    Add: User Model

---
+++ api/models/Users.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamp: true }
+);
+
+const Users = mongoose.model('Users', userSchema);
+export default Users;
