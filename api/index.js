155c220c370527fd4a8e4e30d318041d8974dee0
jitmo_0421 <jerram0421@gmail.com>
2024-01-23 10:56:37 +0800

    Add: Mongoose Setup and Port Setup

--- api/index.js
+++ api/index.js
@@ -1,7 +1,27 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+
+//dotenv config
+dotenv.config();
+
+// Server Setup and Routes
+const port = 5000; // Sub Port
 
 const app = express();
+app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+// Database Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Server Gateway Reponse
+app.listen(process.env.PORT || port, () => {
+  console.log(`API is now online on port: ${process.env.PORT || port}`);
 });
