aa394429486c75db6778b536897ce83ef9e28496
jitmo_0421 <jerram0421@gmail.com>
2024-02-07 09:29:31 +0800

    frontend : added error handling on signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,22 +2,31 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 export default function Signup() {
   const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    const res = await fetch('/api/auth/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-
-    console.log(data);
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      console.log(data);
+      data.success === false ? setError(true) : setError(false);
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
   };
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -47,8 +56,11 @@ export default function Signup() {
           className="bg-slate-100 p-3 rounded-lg"
           onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-85 disabled:opacity-65">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-85 disabled:opacity-65"
+        >
+          {loading ? <span className="loading loading-dots loading-xs"></span> : 'Sign Up'}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -57,6 +69,7 @@ export default function Signup() {
           <span className="text-blue-500 hover:text-blue-700">Sign In</span>
         </Link>
       </div>
+      <p className="text-red-700 mt-5">{error && 'Something went wrong'}</p>
     </div>
   );
 }
