7a4b878df403ce5f074fa6aab87cbbfce995da12
jitmo_0421 <jerram0421@gmail.com>
2024-02-08 11:45:41 +0800

    frontend: added redux persistor

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,13 @@ import './index.css';
 
 //redux toolkit import
 import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
   </Provider>
 );
