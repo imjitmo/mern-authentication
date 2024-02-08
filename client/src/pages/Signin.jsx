1ee67d121ba5b53c1ce9bb1a155bfb790a9f6120
jitmo_0421 <jerram0421@gmail.com>
2024-02-08 11:35:06 +0800

    frontend: added redux toolkit and created slice

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
+//redux imports
+import { useDispatch, useSelector } from 'react-redux';
+import { signInFailure, signInStart, signInSuccess } from '../redux/user/user.slice.js';
+
 export default function Signin() {
   const [formData, setFormData] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-      setError(false);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -23,15 +25,14 @@ export default function Signin() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      setLoading(false);
       if (data.success === false) {
-        setError(true);
+        dispatch(signInFailure(data));
         return;
       }
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError(true);
+      dispatch(signInFailure(error));
     }
   };
   return (
@@ -69,7 +70,7 @@ export default function Signin() {
           <span className="text-blue-500 hover:text-blue-700">Sign Up</span>
         </Link>
       </div>
-      <p className="text-red-700 mt-5">{error && 'Something went wrong'}</p>
+      <p className="text-red-700 mt-5 capitalize">{error ? error.message || 'Something went wrong' : ''}</p>
     </div>
   );
 }
